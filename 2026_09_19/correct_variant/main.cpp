c822159a0cda9a82ef339c0f31946af97f423881
ChichilAlexander <aeksandr.chichil@gmail.com>
2026-09-19 20:10:47 +0300

    idk

---
+++ 2026_09_19/correct_variant/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include "io.hpp"
+#include "sortings.hpp"
+
+int main() {
+    int arr_size;
+    std::cout << "Введите длину масива:";
+    std::cin >> arr_size;
+    int* arr = new int[arr_size];
+    std::cout<<"Введите элементы массива:";
+    for (int i=0;i<arr_size; i++){
+       std::cin >> arr[i];
+    }
+    biv::print_array("Массив до сортировки:", arr, arr_size);
+    biv::bubble_sort(arr, arr_size);
+    biv::print_array("Массив после сортировки:", arr, arr_size);
+    delete[] arr;
+}
+
+
+
+
---
+++ 2026_09_19/sortings.cpp
@@ -0,0 +1,15 @@
+#include "sortings.hpp"
+
+void biv::bubble_sort(int* const arr, const int size) {
+	int temp;
+	for (int i = 0; i<size-1; i++){
+        for (int j = 0; j<size-i-1; j++){
+            if (arr[j]>arr[j+1]){
+                temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+
+            }
+        }
+	}
+}
